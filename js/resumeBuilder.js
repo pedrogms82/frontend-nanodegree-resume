132516ce0fdaaef0c4fe05ed02b7c1a65932a994
pedrogms82 <pedrogms82@gmail.com>
2016-11-21 15:28:32 +0100

    hasta_proyects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -88,3 +88,29 @@ $(document).click(function(loc) {
  console.log(loc);
   logClicks(loc.clientX,loc.clientY);
 });
+
+
+var proyects = [{
+    "title": "proyecto uno",
+    "descripcion" : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis"
+  },{
+    "title" : "proyecto dos",
+    "descripcion" : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis"
+  }];
+
+console.log("Obj proyects", proyects);
+proyects.display = function () {
+
+  for (pro in proyects){
+    $("#projects").append(HTMLprojectStart);
+
+    var fTitle = HTMLprojectTitle.replace("%data%",proyects[pro].title);
+    $(".project-entry").last().append(fTitle);
+
+    var fDes = HTMLprojectTitle.replace("%data%",proyects[pro].descripcion);
+    $(".project-entry").last().append(fDes);
+  }
+
+};
+
+proyects.display();
